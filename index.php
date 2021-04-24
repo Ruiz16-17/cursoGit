9c9786a12487eb3219099cb8627a7d00b217e189
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-04-23 21:44:19 -0500

    negrita y color rojo

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <h1>Hola cursp git</h1>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+        <p>Esto es un p$aacute;rrafo de ejemplo</p>
+    </body>
+</html>
